e5d32f337979baa200299c4f7371f5c54f984b18
heliguang <heliguang163@163.com>
2019-11-03 22:06:38 +0800

    add router:get route

--- gin/api_test.go
+++ gin/api_test.go
@@ -19,3 +19,14 @@ func TestApiPing(t *testing.T) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("Get request result:" + string(body))
 }
+func TestApiUserRoutes(t *testing.T) {
+	pingUrl := "http://" + config.Conf.ApiAddress + "/user/routes?token=admin-token"
+	resp, _ := http.Get(pingUrl)
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Get request result:" + string(body))
+}
